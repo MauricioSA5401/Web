2f331cd2f53f24ee7a78ada796bdda6d2d9854cd
MauricioSA5401 <134406537+MauricioSA5401@users.noreply.github.com>
2024-12-03 12:01:03 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -6,6 +6,6 @@ $dbname = "Residencias_Tesji";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    die("Conexión fallia: " . $conn->connect_error);
 }
 ?>
